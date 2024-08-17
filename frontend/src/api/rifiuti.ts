573d5705c5c110491c88c47c768c247f7e3dffd9
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2024-08-17 17:47:15 +0200

    implementato l'upload di un'immagine per rifiuto

--- frontend/src/api/rifiuti.ts
+++ frontend/src/api/rifiuti.ts
@@ -3,7 +3,7 @@ import { backendClient } from './backendClient';
 export type Rifiuto = {
   codice_cer: string;
   nome: string;
-  imgSrc: string;
+  img_src: string;
 };
 
 export type RifiutoCreate = {
@@ -12,8 +12,11 @@ export type RifiutoCreate = {
   descrizione: string;
 };
 
-export async function registraRifiuto(rifiuto: RifiutoCreate) {
-  return backendClient.post('/rifiuti', rifiuto);
+export async function registraRifiuto(rifiuto: RifiutoCreate, immagine: File) {
+  const formData = new FormData();
+  formData.append('immagine', immagine);
+  formData.append('rifiuto', JSON.stringify(rifiuto));
+  return backendClient.post('/rifiuti', formData);
 }
 
 export async function getRifiuti(): Promise<Rifiuto[]> {
